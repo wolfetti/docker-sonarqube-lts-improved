9a7978159cfb3a11a3ce973abf74c03bb887a367
wolfetti <wolfetti.bsk@gmail.com>
2020-10-12 22:32:22 +0200

    fourth commit

--- run.sh
+++ run.sh
@@ -21,11 +21,13 @@ do
     fi
 done < <(env)
 
+PASS=$(cat ${SONARQUBE_JDBC_PASSWORD_FILE})
+
 exec tail -F ./logs/es.log & # this tail on the elasticsearch logs is a temporary workaround, see https://github.com/docker-library/official-images/pull/6361#issuecomment-516184762
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$(cat ${SONARQUBE_JDBC_PASSWORD_FILE})" \
+  -Dsonar.jdbc.password="$PASS" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
   -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "${sq_opts[@]}" \
